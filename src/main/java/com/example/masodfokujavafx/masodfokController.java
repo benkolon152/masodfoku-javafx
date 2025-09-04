3e75669a3eebc6bd8ecfa35e3a5439f1877977a2
Benkolon152 <v.benasz@gmail.com>
2025-09-04 13:23:59 +0200

    rename projekt elements

--- src/main/java/com/example/masodfokujavafx/masodfokController.java
+++ src/main/java/com/example/masodfokujavafx/masodfokController.java
@@ -3,7 +3,7 @@ package com.example.masodfokujavafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class masodfokController {
     @FXML
     private Label welcomeText;
 
