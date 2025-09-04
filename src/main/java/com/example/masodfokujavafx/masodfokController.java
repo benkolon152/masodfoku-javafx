315a738ea70ddcf99c29a8ab950a66d3021045cd
Benkolon152 <v.benasz@gmail.com>
2025-09-04 14:01:40 +0200

    Solve if 2 diff solutions

--- src/main/java/com/example/masodfokujavafx/masodfokController.java
+++ src/main/java/com/example/masodfokujavafx/masodfokController.java
@@ -1,14 +1,51 @@
 package com.example.masodfokujavafx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class masodfokController {
+    @FXML
+    public TextField textfield_a;
+    @FXML
+    public TextField textfield_b;
+    @FXML
+    public TextField textfield_c;
+    @FXML
+    public Button button_solve;
+    @FXML
+    public ListView listwiev_sol;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSolveButtonClick() {
+        //System.out.println("cig");
+
+        double a = Double.parseDouble(textfield_a.getText());
+        double b = Double.parseDouble(textfield_b.getText());
+        double c = Double.parseDouble(textfield_c.getText());
+
+        double d = b * b - 4.0 * a * c;
+
+        double x1 = (-b + Math.sqrt(d)) / (2.0 * a);
+        double x2 = (-b - Math.sqrt(d)) / (2.0 * a);
+
+        String line1 = "x1 = " + x1;
+        String line2 = "x2 = " + x2;
+
+        List<String> lines = new ArrayList<>();
+        lines.add(line1);
+        lines.add(line2);
+        ObservableList<String> linesFX = FXCollections.observableList(lines);
+
+        listwiev_sol.setItems(linesFX);
     }
 }
